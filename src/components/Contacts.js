6e872218f692e5e8e64f1ea920e127e2a2585a1c
Rafeedmefood <rafidkarim36@gmail.com>
2022-04-09 13:22:16 -0400

    Contacts Page Completed
    Formatted image and bio section, all that is left is actually writing
    out info about myself in the bio section.
    
    Removed one of the cards in the home page, as the "About Me" section
    should be in the contacts page and having another one in the home page
    is redundant.
    
    Improved the look of the site.

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -8,10 +8,10 @@ function Contacts() {
     useEffect(() => {
         setTimeout(function () {
             setIsGeneralBioInfoHidden(true)
-        }, 1000);
+        }, 500);
         setTimeout(function () {
             setIsContactInfoHidden(true)
-        }, 2000);
+        }, 1000);
 
     }, [])
 
@@ -23,11 +23,25 @@ function Contacts() {
         <div className="bg-red-forest w-full h-full min-h-screen min-w-full">
             {isGeneralBioInfoHidden &&
                 <div className="flex justify-center items-center">
-                    <div className="rounded-3xl flex justify-center items-center bg-green-600 p-5 w-[30%] h-[40%] m-[10%] shadow-lg shadow-gray-700">
-                            <img className="rounded-3xl max-h-[400px] max-h-[300px] animate-fade-in-up" src={require("../images/contact.jpg")}/>
-                        <div className="p-5 bg-purple-200 rounded-3xl shadow-lg shadow-gray-700 animate-fade-in-up">
+                    <div
+                        className="rounded-3xl flex justify-center items-center m-[11%]">
+                        <img className="rounded-3xl max-h-[350px] shadow-lg shadow-gray-900 animate-fade-in-up"
+                             src={require("../images/contact.jpg")} alt="Rafid Karim"/>
+                        <div
+                            className="p-5 bg-pink-400 rounded-3xl shadow-lg shadow-gray-900 animate-fade-in-up ml-2.5 h-[350px]">
                             <h1 className=""> About Me </h1>
                             <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                                fugiat nulla pariatur. Excepteur sint occaecat
 
                             </p>
                         </div>
